566980133e9c0c4f67389479eba9a0f506d85c77
Kamapra18 <marioprayoga18@gmail.com>
2025-12-29 09:13:20 +0800

    add google search console verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
-import Snowfall from "./components/Snowfall"
+import Snowfall from "./components/Snowfall";
 import { Exo_2, Montserrat } from "next/font/google";
 
 const exo2 = Exo_2({
@@ -59,6 +59,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="8F1KMMvDIzoKs4jCzzr7bVX3Udy2IhSZjST3gnvRnwk"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${exo2.variable} ${montserrat.variable} antialiased`}>
         <Snowfall />
