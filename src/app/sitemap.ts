53c56511a85e58f74b8c091ec4d55cbd2defc91b
Kamapra18 <marioprayoga18@gmail.com>
2025-12-29 09:37:31 +0800

    add sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,8 +3,22 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://kamapra.my.id",
+      url: "https://kamapra.my.id/",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://kamapra.my.id/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://kamapra.my.id/projects",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
   ];
 }
